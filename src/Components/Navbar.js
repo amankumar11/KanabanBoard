ede4f4a47fce174c7aeb190a79f579f82fbdae5f
Aman Kumar <aman.kumar11112001@gmail.com>
2023-10-16 16:27:43 +0530

    final commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,10 +5,10 @@ import '../styles/Navbar.css';
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <h1 className="main-title">Aman's Board</h1>
+        <h1 className="main-title">Kanaban Board</h1>
         <BsStar className="ml-8 mr-4 nav-star" />
         <h1 className="main-title"> | </h1>
-        <h1 className="main-title">Zuddl & Co.</h1>
+        <h1 className="main-title">Your Company</h1>
     </div>
   )
 }
